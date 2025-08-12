aaa9693f044f57c8529f18c5b581b3fe9ea722a7
morrislm21 <morrislm21@gmail.com>
2025-08-11 21:07:34 -0500

    adding initial page pieces

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Logan Morris</h1>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
