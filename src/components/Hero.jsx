12f357876a122f75f6bd17b9e4dcdd1ec9e32493
morrislm21 <morrislm21@gmail.com>
2025-08-15 10:09:07 -0500

    changing profile pic location for rendering

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,10 @@
 import styles from '../styles/Hero.module.css';
+import profilePic from '../assets/logan-morgan-profile.jpg';
 
 export default function Hero() {
   return (
     <section id="top" className={styles.hero}>
-      <img
-        src="/logan-morgan-profile.jpg"
-        alt="Logan Morris Profile"
-        className={styles.profilePic}
-      />
+      <img src={profilePic} alt="Logan Morris Profile" className={styles.profilePic} />
       <h1 className={styles.name}>Logan Morris</h1>
     </section>
   );
